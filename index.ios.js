e6154e801bcb46c0ffac4be1f64d6bac10d8783c
0dayZh <0day.zh@gmail.com>
2017-11-27 21:11:58 +1100

    Update index tab -> space

--- index.ios.js
+++ index.ios.js
@@ -13,39 +13,39 @@ var Wallet = require('./components/views/wallet.ios');
 var Me = require('./components/views/me.ios');
 
 export default class Hora extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedTab: 'wallet'
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'wallet'
+    };
+  }
 
-    render() {
-        return (
-          <TabBarIOS selectedTab = {this.state.selectedTab}>
-            <TabBarIOS.Item
-                selected = {this.state.selectedTab === 'wallet'}
-                title = 'Wallet'
-                onPress = {() => {
-                    this.setState({
-                        selectedTab: 'wallet',
-                    });
-                }}>
-                <Wallet/>
-            </TabBarIOS.Item>
-            <TabBarIOS.Item
-                selected = {this.state.selectedTab === 'me'}
-                title = 'Me'
-                onPress = {() => {
-                    this.setState({
-                        selectedTab: 'me',
-                    });
-                }}>
-            <Me/>
-            </TabBarIOS.Item>
-          </TabBarIOS>
-        );
-    }
+  render() {
+    return (
+      <TabBarIOS selectedTab = {this.state.selectedTab}>
+        <TabBarIOS.Item
+          selected = {this.state.selectedTab === 'wallet'}
+          title = 'Wallet'
+          onPress = {() => {
+            this.setState({
+              selectedTab: 'wallet',
+            });
+          }}>
+          <Wallet/>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          selected = {this.state.selectedTab === 'me'}
+          title = 'Me'
+          onPress = {() => {
+            this.setState({
+              selectedTab: 'me',
+            });
+          }}>
+        <Me/>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
 }
 
 AppRegistry.registerComponent('Hora', () => Hora);
