c697fa65dd27e167eb6a1ba2b2ef76cd2ae20d7f
whoIsNeoHuang <neo_huang@olympuslabs.io>
2017-12-03 20:14:23 +0800

    Add asset list ui

--- components/views/wallet.js
+++ components/views/wallet.js
@@ -6,6 +6,10 @@ import {
   View,
   Text
 } from 'react-native';
+import {
+  List,
+  ListItem
+} from 'react-native-elements';
 
 var styles = StyleSheet.create({
   description: {
@@ -21,14 +25,50 @@ var styles = StyleSheet.create({
   }
 });
 
+const list = [
+  {
+    symbol: 'eth',
+    name: 'Ethereum',
+    avatar: 'https://files.coinmarketcap.com/static/img/coins/32x32/ethereum.png',
+    amount: 2.34
+  },
+  {
+    symbol: 'knc',
+    name: 'Kyber Network',
+    avatar: 'https://files.coinmarketcap.com/static/img/coins/32x32/kyber-network.png',
+    amount: 3201
+  },
+  {
+    symbol: 'snt',
+    name: 'Status',
+    avatar: 'https://files.coinmarketcap.com/static/img/coins/32x32/status.png',
+    amount: 3431
+  },
+];
+
 class Wallet extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.description}>
-          Welcome to Olympus Wallet!
-        </Text>
-      </View>
+      <List style={{flex: 1}} containerStyle={{borderTopWidth: 0, borderBottomWidth: 0, borderBottomColor: 'transparent'}}>
+        {
+          list.map((l, i) => (
+            <ListItem
+              roundAvatar
+              hideChevron={true}
+              avatar={{uri: l.avatar}}
+              key={i}
+              title={l.symbol.toUpperCase()}
+              subtitle={l.name}
+              rightTitle={l.amount.toString()}
+            />
+          ))
+        }
+      </List>
+      // <View style={styles.container}>
+      //   <Text style={styles.description}>
+      //     Welcome to Olympus Wallet!
+      //   </Text>
+      // </View>
     );
   }
 }
