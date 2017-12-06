7e69c194af1f375ac570aa4d754a2c70e4c8c4fa
whoIsNeoHuang <neo_huang@olympuslabs.io>
2017-12-06 20:57:07 +0800

    Update me view

--- src/views/me.js
+++ src/views/me.js
@@ -4,8 +4,14 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  ScrollView,
+  Image
 } from 'react-native';
+import {
+  List,
+  ListItem
+} from 'react-native-elements';
 
 var styles = StyleSheet.create({
   description: {
@@ -21,16 +27,38 @@ var styles = StyleSheet.create({
   }
 });
 
-class Me extends Component {
+const list = [
+  {
+    icon: <Image/>,
+    title: 'Wallet Management'
+  },
+  {
+    icon: <Image/>,
+    title: 'Olympus Project'
+  },
+  {
+    icon: <Image/>,
+    title: 'Team'
+  }
+];
+
+class MeView extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.description}>
-          This could be Me!
-        </Text>
-      </View>
+      <ScrollView>
+        <list>
+        {
+          list.map((l, i) => (
+            <ListItem
+              avatar={{image: l.icon}}
+              title={l.title}
+            />
+          ))
+        }
+        </list>
+      </ScrollView>
     );
   }
 }
 
-module.exports = Me;
+export default MeView;
