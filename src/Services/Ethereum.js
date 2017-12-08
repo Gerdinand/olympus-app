479c9afd9b2b97d68ae789e920993a1d76d9aafe
whoIsNeoHuang <neo_huang@olympuslabs.io>
2017-12-08 23:19:01 +0800

    Add getBalance function && Rename Ethereum service

---
+++ src/Services/Ethereum.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import Web3 from 'web3';
+import Wallet from 'ethereumjs-wallet-react-native';
+
+const promisify = (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );
+
+class EthereumService {
+  constructor() {
+    this.rpc = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/xiNNVkYQ6V3IsiPWTTNT ", 9000));
+  }
+
+  version() {
+    return this.rpc.version.api;
+  }
+
+  async getBalance(address) {
+    var balance;
+    const wei = promisify(cb => this.rpc.eth.getBalance(address, cb));
+    try {
+      balance = web3.fromWei(await wei, 'ether');
+      return balance;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default EthereumService;
