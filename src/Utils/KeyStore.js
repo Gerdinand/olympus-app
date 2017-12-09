9a6bd8c6d15ea97293ad0a8b406747eace92d699
whoIsNeoHuang <neo_huang@olympuslabs.io>
2017-12-10 01:38:30 +0800

    Remove useless log

--- src/Utils/KeyStore.js
+++ src/Utils/KeyStore.js
@@ -3,8 +3,6 @@
 import * as Keychain from 'react-native-keychain';
 
 export async function saveItem(key, value) {
-  console.log("key: " + key);
-  console.log("value: " + value);
   await Keychain.setGenericPassword(key, value);
 }
 
