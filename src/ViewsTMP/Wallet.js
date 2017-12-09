d3d4af744f908165410e353ec6de1ae56521bace
0dayZh <0day.zh@gmail.com>
2017-12-10 03:25:27 +0800

    Update wallet

---
+++ src/ViewsTMP/Wallet.js
@@ -0,0 +1,109 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView
+} from 'react-native';
+import {
+  List,
+  ListItem
+} from 'react-native-elements';
+
+import WalletHeader from '../UIElements/WalletHeader';
+import EthereumWalletService from '../Services/EthereumWallet';
+
+var styles = StyleSheet.create({
+  description: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#FFFFFF'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  }
+});
+
+const list = [
+  {
+    symbol: 'eth',
+    name: 'Ethereum',
+    avatar: 'https://files.coinmarketcap.com/static/img/coins/32x32/ethereum.png',
+    amount: 2.34
+  },
+  {
+    symbol: 'knc',
+    name: 'Kyber Network',
+    avatar: 'https://files.coinmarketcap.com/static/img/coins/32x32/kyber-network.png',
+    amount: 3201
+  },
+  {
+    symbol: 'snt',
+    name: 'Status',
+    avatar: 'https://files.coinmarketcap.com/static/img/coins/32x32/status.png',
+    amount: 3431
+  },
+];
+
+class WalletView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.eth = EthereumWalletService.getInstance();
+    this.state = {
+      name: null,
+      address: null,
+    };
+  }
+
+  componentWillMount() {
+    const wallet = this.eth.wallet;
+    console.log(wallet);
+    this.setState({ name: wallet.name, address: wallet.address });
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <ScrollView style={{backgroundColor: 'white'}}>
+        <WalletHeader
+          name={this.state.name}
+          address={this.state.address}
+          balance={"$ " + 0}
+        />
+        <List style={{height: 578}} containerStyle={{borderTopWidth: 0, borderBottomWidth: 0, borderBottomColor: 'transparent'}}>
+        {
+          list.map((l, i) => (
+            <ListItem
+              roundAvatar
+              hideChevron={true}
+              avatar={{uri: l.avatar}}
+              key={i}
+              title={l.symbol.toUpperCase()}
+              subtitle={l.name}
+              rightTitle={l.amount.toString()}
+              rightTitleStyle={{fontWeight:'bold', color:'#4A4A4A'}}
+              onPress={() => {
+                console.log(l.symbol);
+                navigation.navigate('WalletDetail', { title: l.symbol })
+              }}
+            />
+          ))
+        }
+        </List>
+      </ScrollView>
+    );
+  }
+}
+
+export default WalletView;
