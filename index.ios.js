ac05db54aea39d8435553a68435ac39e7e540131
0dayZh <0day.zh@gmail.com>
2017-12-10 22:37:51 +0800

    Update Tabs

--- index.ios.js
+++ index.ios.js
@@ -15,8 +15,7 @@ import {
 import { TabNavigator } from 'react-navigation';
 import { EventRegister } from 'react-native-event-listeners';
 
-import WalletTab from './src/Tabs/Wallet';
-import MeTab from './src/Tabs/Me';
+import { WalletTab, MeTab } from './src/Tabs';
 import { Welcome } from './src/Containers';
 import { WalletService } from './src/Services';
 
