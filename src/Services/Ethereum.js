3d5867116323764d72ae489ef40f144a14773f9c
0dayZh <0day.zh@gmail.com>
2017-12-10 17:46:44 +0800

    Add static instance and get balance method

--- src/Services/Ethereum.js
+++ src/Services/Ethereum.js
@@ -1,8 +1,32 @@
 'use strict';
 
+import '../../shim.js'
+
+import Web3 from 'web3';
+import EthJs from 'ethereumjs-wallet-react-native';
+import Tx from 'ethereumjs-tx'
+
 class EthereumService {
 
+  constructor() {
+    this.rpc = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/0BRKxQ0SFvAxGL72cbXi", 9000));
+  }
 
+  static myInstance = null;
+
+  static getInstance() {
+    if (this.myInstance == null) {
+      this.myInstance = new EthereumService();
+    }
+
+    return this.myInstance;
+  }
+
+  async getBalance(address) {
+    const balance = await this.rpc.eth.getBalance(address);
+    console.log("balance: " + balance);
+    return balance;
+  }
 }
 
 export default EthereumService;
