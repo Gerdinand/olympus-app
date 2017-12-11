1d47065a2ab4ab51e19db9953aa53961aa9e22b4
0dayZh <0day.zh@gmail.com>
2017-12-11 21:48:17 +0800

    Add send tx implementation

--- src/Utils/Converter.js
+++ src/Utils/Converter.js
@@ -2,6 +2,83 @@
 
 import BigNumber from 'bignumber.js'
 
+function acceptableTyping(number) {
+  // ends with a dot
+  if (number.length > 0 && number[number.length - 1] == ".") {
+    return true
+  }
+
+  // TODO refactor format
+  // zero suffixed with real number
+  // if (number.length > 0 && number[number.length - 1] == "0") {
+  //   for (var i = 0; i < number.length; i++) {
+  //     if (number[i] == ".") {
+  //       return true
+  //     }
+  //   }
+  // }
+  return false
+}
+
+export function toTWei(number) {
+  var bigNumber = new BigNumber(number)
+  if (bigNumber == 'NaN' || bigNumber == 'Infinity') {
+    return number
+  } else if (acceptableTyping(number)) {
+    return number
+  } else {
+    return bigNumber.times(1000000000000000000).toString()
+  }
+}
+
+export function toT(number, precision) {
+  var bigNumber = new BigNumber(number)
+  var result
+  if (bigNumber == 'NaN' || bigNumber == 'Infinity') {
+    return number
+  } else if (acceptableTyping(number)) {
+    return number
+  } else {
+    result = bigNumber.div(1000000000000000000)
+  }
+  if (precision) {
+    return result.toFixed(precision)
+  } else {
+    return result.toString()
+  }
+}
+
+export function toEther(number) {
+  var bigNumber = new BigNumber(number)
+  if (bigNumber == 'NaN' || bigNumber == 'Infinity') {
+    return "0"
+  } else {
+    return bigNumber.dividedBy(1000000000000000000).toString()
+  }
+}
+
+export function gweiToWei(number) {
+  var bigNumber = new BigNumber(number)
+  if (bigNumber == 'NaN' || bigNumber == 'Infinity') {
+    return number
+  } else if (acceptableTyping(number)) {
+    return number
+  } else {
+    return bigNumber.times(1000000000).toString()
+  }
+}
+
+export function weiToGwei(number) {
+  var bigNumber = new BigNumber(number)
+  if (bigNumber == 'NaN' || bigNumber == 'Infinity') {
+    return number
+  } else if (acceptableTyping(number)) {
+    return number
+  } else {
+    return bigNumber.div(1000000000).toString()
+  }
+}
+
 export function numberToHex(number) {
   return "0x" + (new BigNumber(number)).toString(16)
 }
