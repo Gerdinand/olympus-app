002cc20f4ea8426c19dd1450526bdd5317ddfa43
Jerome Chen <jerome@secondcompany.nl>
2018-01-23 08:57:17 +0800

    local

--- android/app/src/main/java/com/hora/MainApplication.java
+++ android/app/src/main/java/com/hora/MainApplication.java
@@ -3,6 +3,7 @@ package com.hora;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.remobile.toast.RCTToastPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.peel.react.TcpSocketsModule;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTToastPackage(),
             new RCTCameraPackage(),
             new UdpSocketsModule(),
             new TcpSocketsModule(),
