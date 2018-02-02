47181c74ab4707525ff00ba7c782aff6e95e0535
Jerome Chen <jerome_chen@olympuslabs.io>
2018-02-03 00:48:59 +0800

    Switched to ropsten and upgraded kyber contract invokes.

--- src/common/SendTx.js
+++ src/common/SendTx.js
@@ -37,7 +37,7 @@ class SendTx extends Component {
         to: _.state.addr,
         value: web3.utils.numberToHex(web3.utils.toWei(_.state.value, 'ether')),
         data: '',
-        chainId: 3,
+        chainId: 3, // not we use ropsten, not kovan 42,
       };
       let tx = new Tx(rawTx);
       tx.sign(Buffer(_.state.privKey, 'hex'));
