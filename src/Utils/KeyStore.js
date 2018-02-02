27ac0ae30cd13cf5576b45e6fec63a7258b9486d
Jerome Chen <jerome@secondcompany.nl>
2018-02-02 14:11:41 +0800

    Merged.

--- src/Utils/KeyStore.js
+++ src/Utils/KeyStore.js
@@ -3,11 +3,14 @@
 import * as Keychain from 'react-native-keychain';
 
 export async function saveItem(key, value) {
-  await Keychain.setGenericPassword(key, value);
+  const result = await Keychain.setGenericPassword(key, value);
+  debugger;
+  return result;
 }
 
 export async function readItem(key) {
   const json = await Keychain.getGenericPassword();
+  debugger;
   if (json.username == key) {
     return json.password;
   } else {
