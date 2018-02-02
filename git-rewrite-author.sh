ee0782d609d30eaa57bb7c14a1c4ff986d4af042
Jerome <jerome_chen@olympuslabs.io>
2018-02-03 00:59:11 +0800

    Typo correction and use constant for ChainID.

---
+++ git-rewrite-author.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="jerome@secondcompany.nl"
+CORRECT_NAME="Jerome Chen"
+CORRECT_EMAIL="jerome_chen@olympuslabs.io"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
