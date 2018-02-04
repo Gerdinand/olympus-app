21850b27532896b15f614285bd8c0c1fda0cf0b0
Jerome Chen <jerome_chen@olympuslabs.io>
2018-02-04 22:15:08 +0800

    Added logo to launch screen. (iOS).

--- android/app/src/main/java/com/olympus/MainApplication.java
+++ android/app/src/main/java/com/olympus/MainApplication.java
@@ -3,6 +3,7 @@ package com.olympus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.peel.react.TcpSocketsModule;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new VectorIconsPackage(),
             new UdpSocketsModule(),
             new TcpSocketsModule(),
