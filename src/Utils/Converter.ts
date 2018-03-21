78a117e72aad213ea908115236ab56aae1d48e17
Abel Bordonado Lillo <abel.bordonado@secondcompany.cn>
2018-03-21 19:42:59 +0800

    Support to amount of 1.00200 type

--- src/Utils/Converter.ts
+++ src/Utils/Converter.ts
@@ -110,10 +110,14 @@ export function restrictTextToNumber(text: string): { text: string, textCorrect:
 }
 
 export function filterStringLessThanNumber(value: string, max: number) {
-
-  if (value === '' || value === '.' || (/^(\d)*\.(0+)?$/.test(value)) || !Number(value)) {
-    return value; // The cast would return no decimals, but that could be annoying
-  }
+  const numberDotCeros = /^(\d)*\.(0+)?$/; // 1.000
+  const numberDotDecimalCeros = /^(\d)*\.(\d)*(0+)?$/;
+  if (value === ''
+    || value === '.'
+    || (numberDotCeros.test(value))
+    || numberDotDecimalCeros.test(value)) { // !Number(value)) {
+    return value;
+  } // The cast would return no decimals, but that could be annoying
 
   return Math.min(Number(value), max).toString();
 }
