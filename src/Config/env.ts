84614f8a82ec79857709c7f9fe2b8d51754ad4e6
Abel Bordonado Lillo <abel.bordonado@secondcompany.cn>
2018-03-25 18:53:59 +0800

    Set server url for staging production

--- src/Config/env.ts
+++ src/Config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  production: { server: 'http://111.230.196.228:8080' }, // TODO change when server is develop
-  development: { server: 'http://192.168.2.88:8080' },
+  production: { server: 'http://services.aireach.com.cn' },
+  development: { server: 'http://services.aireach.com.cn' },
   local: { server: 'http://localhost:8080' },
 };
