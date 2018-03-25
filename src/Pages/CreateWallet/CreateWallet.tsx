80a6db8dde18dfd55008666595e276c9b2cf678e
Gerdinand <gerdinand.hardeman@outlook.com>
2018-03-25 21:10:38 +0800

    fix padding and ci

--- src/Pages/CreateWallet/CreateWallet.tsx
+++ src/Pages/CreateWallet/CreateWallet.tsx
@@ -77,11 +77,10 @@ class CreateWalletView extends React.Component<ReduxProps, InternalState> {
   private async createWallet() {
 
     if (this.isValidate()) {
-      const name = this.state.name;
       const password = this.state.password1;
       try {
         /*const json = */
-        await this.eth.generateV3Wallet(name, password);
+        await this.eth.generateV3Wallet(password);
         const wallet = await WalletService.getInstance().wallet;
         this.props.setWallet(wallet);
       } catch (e) {
--- src/Pages/ImportWallet/partials/ImportWalletHeaderStyle.tsx
+++ src/Pages/ImportWallet/partials/ImportWalletHeaderStyle.tsx
@@ -13,6 +13,7 @@ export default StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
+    paddingBottom: 8,
   },
   selected: {
     borderBottomWidth: 1,
